61d311d8efd080947e88f7390770fc1327e52e54
Ader613 <weinader@gmail.com>
2024-08-21 12:44:55 -0400

    name and numbers

---
+++ name.py
@@ -0,0 +1,15 @@
+names = { “wei", “nikki", “akua" }
+for name in names:
+newName = capitalize(name)
+print newName
+
+# Function to capitalize a name
+def capitalize(name):
+    return name.capitalize()
+
+# Get user input for a name
+user_name = input("Enter a name to capitalize: ")
+
+# Capitalize and print the user input
+capitalized_user_name = capitalize(user_name)
+print(capitalized_user_name)
